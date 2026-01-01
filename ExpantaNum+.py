e496e99f96ee91130cb0e9bdf75149af49a5f54f
hamster624 <viktors.jirgen@gmail.com>
2026-01-01 20:36:21 +0200

    Fix format

--- ExpantaNum+.py
+++ ExpantaNum+.py
@@ -665,7 +665,7 @@ def format(num, decimals=decimals, small=False):
         return "1/" + format(inv, decimals)
     elif lt(num_correct, 1): return regular_format(n, decimals + (2 if small else 0))
     elif lt(num_correct, 1000): return regular_format(n, decimals)
-    elif lt(num_correct, 1e9): return comma_format(n)
+    elif lt(num_correct, MAX_SAFE_INT): return comma_format(n)
     elif lt(num_correct, [0, 10000000000, 3]):
         bottom = n[1]
         rep = n[2] - 1
@@ -876,7 +876,7 @@ def suffix(num, small=False):
         return "1/" + _suffix(inv)
     elif lt(num_correct, 1): return regular_format(n, decimals + (2 if small else 0))
     elif lt(num_correct, 1000): return regular_format(n, decimals)
-    elif lt(num_correct, 1e9): return _suffix(n)
+    elif lt(num_correct, MAX_SAFE_INT): return _suffix(n)
     elif lt(num_correct, [0, max_suffix, 1]): return _suffix(n)
     elif lt(num_correct, [0, max_suffix, 2]):
         bottom = n[1]
@@ -918,7 +918,7 @@ def suffix(num, small=False):
         n_val = n[2] + 1
         if gte(num_correct, [0, 10, n_val]):
             n_val += 1
-        return "F" + format(n_val, decimals)
+        return "F" + suffix(n_val, decimals)
     elif lt(num_correct, [0, 10000000000, 8, 999998]): return regular_format([0, pol['bottom']], precision3) + "G" + _suffix(pol['top'], 0)
     elif lt(num_correct, [0, 10000000000, 8, 8, 3]):
         rep = n[4]
